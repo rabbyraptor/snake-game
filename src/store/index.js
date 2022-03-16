f21a7df92b2d2dc375a2927bf2870b31cede3047
Mathias Hammer <rabbyraptor@hotmail.com>
2022-03-16 14:43:17 +0100

    Add API for getting and updating highscore list

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+const request = require("axios");
+const endpoint = "mBqKR5zX";
+const token = "4jU9UPNM5tu4upDtVz442g";
 
 Vue.use(Vuex)
 
@@ -10,8 +13,30 @@ export default new Vuex.Store({
       score: 0,
     },
     elapsedTime: 0,
+    highscores: [],
   },
   getters: {
+    player(state) {
+      return state.player;
+    },
+    elapsedTime(state) {
+      return state.elapsedTime;
+    },
+    highscores(state) {
+      function compare(a, b) {
+        if (a.score < b.score) {
+          return 1;
+        }
+        if (a.score > b.score) {
+          return -1;
+        }
+        return 0;
+      }
+
+      let sortedScores = state.highscores.sort(compare);
+      let scores = sortedScores.slice(0, 10)
+      return scores;
+    }
   },
   mutations: {
     incrementScore(state, score) {
@@ -19,9 +44,38 @@ export default new Vuex.Store({
     },
     setPlayerName(state, playerName) {
       state.player.name = playerName;
+    },
+    resetGame(state) {
+      state.player.score = 0;
+      state.elapsedTime = 0;
+    },
+    setHighscoreList(state, highscores) {
+      state.highscores = highscores;
+    },
+    addScoreToHighscore(state) {
+      state.highscores.push({ playerName: state.player.name, score: state.player.score })
     }
   },
   actions: {
+    async getHighscoreList({ commit }) {
+      await request
+        .get("https://app.fakejson.com/q/" + endpoint + "?token=" + token)
+        .then((res) => {
+          commit('setHighscoreList', res.data.highscores)
+        });
+    },
+    async submitHighscore({ commit, state }) {
+      commit('addScoreToHighscore');
+      await request
+        .patch("https://app.fakejson.com/q/" + endpoint, {
+          token: token, data: { highscores: state.highscores }
+        })
+        .then((res) => {
+          setTimeout(() => {
+            commit('setHighscoreList', res.data.highscores)
+          }, 2000)
+        });
+    }
   },
   modules: {
   }
